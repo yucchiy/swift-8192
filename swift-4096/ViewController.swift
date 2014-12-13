701170bb898677f547c2e5ae1c87dbbc2fc2fe7b
Yuichiro Mukai <y.iky917@gmail.com>
2014-12-13 17:02:26 +0900

    スタートボタンを設置

--- swift-4096/ViewController.swift
+++ swift-4096/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onClickGameStartButton(sender: UIButton) {
+        NSLog("called onClickGameStartButton")
+    }
 }
 
